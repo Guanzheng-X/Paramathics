6d63c2d0e240b741b1ba1c1a01e655d1ca8421cc
aadityashukla1 <aadityashukla@yahoo.com>
2016-03-11 22:13:17 -0500

    Update MatrixMultiplicationFunction.c

--- Aaditya/MatrixMultiplicationFunction.c
+++ Aaditya/MatrixMultiplicationFunction.c
@@ -1,3 +1,10 @@
+// Flipt will inject fault in this method
+// Below is the example of how to compile it with corrupt library
+//$LLVM_BUILD_PATH/bin/clang  -g -emit-llvm work.c  -c -o work.bc 
+//$LLVM_BUILD_PATH/bin/llvm-link $FLIPIT_PATH/include/FlipIt/corrupt/corrupt.bc work.bc  -o crpt_work.bc
+//$LLVM_BUILD_PATH/bin/opt -load $FLIPIT_PATH/lib/libFlipItPass.so -FlipIt -srcFile "work.c" -singleInj 1 -prob 0.95 -byte -1 -arith 1 -ctrl 0 -ptr 0 -funcList "" crpt_work.bc  -o final.bc
+//$LLVM_BUILD_PATH/bin/clang  -g -c final.bc -o final.o 
+
 void multiplication(int a[][10],int b[][10],int mult[][10],int r1,int c1,int r2,int c2)
 {
     int i,j,k;
