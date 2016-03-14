39d369084eb7cfbc760b9f86809b3d2a2c6a530e
Yue <13571804530@163.com>
2016-03-14 12:16:45 -0400

    Added files via upload

--- Yue/main.cpp
+++ Yue/main.cpp
@@ -40,69 +40,7 @@ void conjugate_gradient_test() {
     for (std::vector<double>::iterator it = x.begin() ; it != x.end(); ++it)
         std::cout << ' ' << *it;
     std::cout<<endl;
-     //Test Case 1
-//    SparseMatrix a(4, 2);
-//    a.set_element(0, 0, 1.0);
-//    a.set_element(0, 1, 2.0);
-//    
-//    a.set_element(1, 0, 2.0);
-//    a.set_element(1, 1, -3.0);
-//    
-//    a.set_element(2, 0, 4.0);
-//    a.set_element(2, 1, -1.0);
-//    
-//    a.set_element(3, 0, -5.0);
-//    a.set_element(3, 1, 2.0);
-//    
-//    // Suppose solution is (2, 3).
-//    std::vector<double> b(4);
-//    b[0] = 3.0;  // 8.0
-//    b[1] = 2.0;  // -5.0
-//    b[2] = -4.0;
-//    b[3] = 5.0;
-//    
-//    std::vector<double> x(2);
-//    x[0] = x[1] = 0.0;
-//    
-//    Math::conjugate_gradient(a, &b[0], 2, &x[0]);
-//        for (std::vector<double>::iterator it = x.begin() ; it != x.end(); ++it)
-//            std::cout << ' ' << *it;
-//        std::cout<<endl;
-//    std::vector<double> c(4);
-//    a.multiply_column(&x[0], &c[0]);
-//    
-//    printf("x: %lf %lf\n", x[0], x[1]);
-//    
-//    for (int i = 0; i < 4; i++) {
-//        printf(" %lf(%lf)", c[i], b[i]);
-//    }
-//    printf("\n");
-//    
-//    // Test Case 2
-//    a = SparseMatrix(1, 2);
-//    a.set_element(0, 0, 1.0);
-//    a.set_element(0, 1, 2.0);
-//    
-//    b.resize(1);
-//    b[0] = 3.0;
-//    
-//    // x[0] = x[1] = 0.0;
-//    x[0] = 7.0;
-//    x[1] = -3.0;
-//    
-//    Math::conjugate_gradient(a, &b[0], 2, &x[0]);
-//    
-//    c.resize(1);
-//    a.multiply_column(&x[0], &c[0]);
-//    
-//    printf("x: %lf %lf\n", x[0], x[1]);
-//    
-//    for (int i = 0; i < 1; i++) {
-//        printf(" %lf(%lf)", c[i], b[i]);
-//    }
-//    printf("\n");
-//    
-//    printf("} conjugate_gradient_test\n");
+
 }
 
 int main() {
