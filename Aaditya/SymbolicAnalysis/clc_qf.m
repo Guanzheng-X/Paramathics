99fb2153a4f501c1476a10d0b61897d6368ee105
aadityashukla1 <aadityashukla@yahoo.com>
2016-09-20 20:06:25 -0400

    Update clc_qf.m

--- Aaditya/SymbolicAnalysis/clc_qf.m
+++ Aaditya/SymbolicAnalysis/clc_qf.m
@@ -13,6 +13,7 @@ qft = sym('QfT');
 qlt = sym('QlT');
 qct = sym('QcT');
 rlt = sym('RlT');
+rlt1 = sym('RlT1');
 pft = sym('PfT');
 plt = sym('PlT');
 
@@ -34,15 +35,15 @@ rl = (b - (a * xf));
 ql = a * pf;
 alpl = (rlt * pf) / (pft * qlt);
 xl = xf + (alpl * pf);
-rl = rf - (alpl * ql);
-btl = - (rlt * ql) / (pft * qlt);
-pl = rl + ( btl * pf);
+rl1 = rl - (alpl * ql);
+btl = - (rlt1 * ql) / (pft * qlt);
+pl = rl1 + ( btl * pf);
 
 % iteration of cg
 qc = a * pl;
-alpc = (rl^2) / (plt *qct);
+alpc = (rl1^2) / (plt *qct);
 xc = xl + (alpc * pl);
-rc = rl - (alpc * qc);
+rc = rl1 - (alpc * qc);
 btc = (rc^2) / (rf^2);
 pc = rc + (btc * pf);
 
